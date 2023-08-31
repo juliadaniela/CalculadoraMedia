f5966a951459dff5037fcbf76ecbb4aa47c7f60d
juliadaniela <90982566+juliadaniela@users.noreply.github.com>
2023-08-31 12:46:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var nome= "Júlia"
+var notaDoPrimeiroBimestre = 9
+var notaDoSegundoBimestre = 7
+var notaDoTerceiroBimestre = 4
+var notaDoQuartoBimestre = 2
+
+var notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre) / 4
+var notaFixada =  notaFinal.toFixed(1)
+console.log("Bem vinda, " + nome)
+console.log(notaFixada)
+
+//vocês são demais!!!
